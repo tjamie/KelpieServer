008fd50f386b4cd17d6061366eab4d88ce387673
Taylor <17496150+myrcheburgers@users.noreply.github.com>
2023-05-09 15:56:24 -0400

    initial API

--- KelpieServer/Program.cs
+++ KelpieServer/Program.cs
@@ -1,3 +1,6 @@
+using KelpieServer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<EF_DataContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("DbContext")));
+//builder.Services.AddDbContext<DbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DbContext")));
 
 var app = builder.Build();
 
