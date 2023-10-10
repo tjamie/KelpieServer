f8cd1ee2eae5a1a43ca82fc8bfe2c7b89f93f064
Taylor <17496150+myrcheburgers@users.noreply.github.com>
2023-10-10 18:02:50 -0400

    Add authorization to projects and datapoints controllers

--- KelpieServer/Controllers/DatapointsController.cs
+++ KelpieServer/Controllers/DatapointsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,7 @@ namespace KelpieServer.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class DatapointsController : ControllerBase
     {
         private readonly EF_DataContext _context;
